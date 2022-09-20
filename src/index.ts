48e887e0468c7cd9185e0307dec42897a6ed038d
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-09-20 15:57:49 +0200

    Conexion con la base de datos realizada

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 require("dotenv").config();
 import { GatewayIntentBits } from "discord.js";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 import config from "../slappey.json";
 import DiscordClient from "./client/client";
 import { registerCommands, registerEvents } from "./utils/registry";
@@ -12,4 +14,12 @@ const client = new DiscordClient({
   await registerCommands(client, "../commands");
   await registerEvents(client, "../events");
   await client.login(process.env.DJS_BOT_TOKEN);
+  await createConnection({
+    type: "mariadb",
+    host: process.env.MARIADB_DB_HOST,
+    port: 3306,
+    username: process.env.MARIADB_DB_USERNAME,
+    password: process.env.MARIADB_DB_PASSWORD,
+    database: process.env.MARIADB_DB_DATABASE
+  });
 })();
